223c8d78fd846580401c3beb40430446339e0a07
David Glass <david@inspiredagency.co.uk>
2015-09-18 13:07:31 +0100

    Added some functionality to the cache

--- Wired.Caching/ICacheService.cs
+++ Wired.Caching/ICacheService.cs
@@ -5,13 +5,26 @@ namespace Wired.Caching
     public interface ICacheService
     {
         /// <summary>
+        /// Determines if an item is in the cache by key
+        /// </summary>
+        /// <param name="key">The key of the item in the cache</param>
+        /// <returns></returns>
+        bool IsInCache(string key);
+
+        /// <summary>
         /// Gets an item from cache or calls the callback methos and stores the result in the cache
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="cacheKey">The key of the item in teh cache</param>
-        /// <param name="getItemCallback">Delegate callback to get the item if needed</param>
+        /// <param name="key">The key of the item in the cache</param>
+        /// <param name="getItemDelegate">Delegate callback to get the item if needed</param>
         /// <param name="duration">The duration in seconds to store the item in the cache</param>
         /// <returns>The cached item or result of the callback if item is not in the cache</returns>
-        T Get<T>(string cacheKey, Func<T> getItemCallback, int duration) where T : class;
+        T Get<T>(string key, Func<T> getItemDelegate, int duration) where T : class;
+
+        /// <summary>
+        /// Removes an item from the cache
+        /// </summary>
+        /// <param name="key">The key of the item in the cache</param>
+        void RemoveFromCache(string key);
     }
 }
--- Wired.Caching/MemoryCache.cs
+++ Wired.Caching/MemoryCache.cs
@@ -11,26 +11,46 @@ namespace Wired.Caching
         private static readonly object SyncObject = new object();
 
         /// <summary>
+        /// Determines if an item is in the cache by key
+        /// </summary>
+        /// <param name="key">The key of the item in the cache</param>
+        /// <returns></returns>
+        public bool IsInCache(string key)
+        {
+            return MemoryCache.Default.Contains(key);
+        }
+
+        /// <summary>
         /// Gets an item from cache or calls the callback method and stores the result in the cache
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="cacheKey">The key of the item in teh cache</param>
-        /// <param name="getItemCallback">Delegate callback to get the item if needed</param>
+        /// <param name="key">The key of the item in the cache</param>
+        /// <param name="getItemDelegate">Delegate callback to get the item if needed</param>
         /// <param name="duration">The duration in seconds to store the item in the cache</param>
         /// <returns>The cached item or result of the callback if item is not in the cache</returns>
-        public T Get<T>(string cacheKey, Func<T> getItemCallback, int duration) where T : class
+        public T Get<T>(string key, Func<T> getItemDelegate, int duration) where T : class
         {
+            
             lock (SyncObject)
             {
-                var item = MemoryCache.Default.Get(cacheKey) as T;
+                var item = MemoryCache.Default.Get(key) as T;
 
                 if (item != null) return item;
 
-                item = getItemCallback();
+                item = getItemDelegate();
 
-                MemoryCache.Default.Add(cacheKey, item, DateTime.Now.AddSeconds(duration));
+                MemoryCache.Default.Add(key, item, DateTime.Now.AddSeconds(duration));
                 return item;
             }
+        }
+
+        /// <summary>
+        /// Removes an item from the cache
+        /// </summary>
+        /// <param name="key">The key of the item in the cache</param>
+        public void RemoveFromCache(string key)
+        {
+            MemoryCache.Default.Remove(key);
         }
     }
 }
